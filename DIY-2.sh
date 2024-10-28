307196b01d96bf33c1af12667fe23e709afd5d1c
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-10-28 19:09:36 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -3,7 +3,7 @@
 
 
 # 1-设置默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-netgear/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-netgear/g' feeds/luci/collections/luci/Makefile
 
 # 2-设置管理地址
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
