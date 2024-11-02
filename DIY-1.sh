248e7915a076d590b418cd1302bf045d481392bc
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-11-02 21:52:37 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -2,9 +2,6 @@
 #=========================================================================================================================
 
 
-# 1-添加ShadowSocksR Plus+插件
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 2-添加添加PassWall插件
+# 1-添加添加PassWall插件
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
