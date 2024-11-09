8915e93463c2f42b6b1596bc97d268d709de7b95
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-11-09 10:02:14 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -5,3 +5,10 @@
 # 1-添加添加PassWall插件
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+rm -rf feeds/luci/applications/luci-app-transmission
+rm -rf feeds/packages/net/transmission
+rm -rf feeds/packages/net/transmission-web-control
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/luci/applications/luci-app-msd_lite
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git Aaron https://github.com/Aaron3299/opackage.git' >> feeds.conf.default
