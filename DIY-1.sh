a07febfc6721a8b6e889513965b9944d03aa0aaa
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2026-05-24 16:49:49 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,18 +1,35 @@
 # DIY-1 此脚本功能：添加外部插件
 # =======================================================================================================================
+# 0-删除部分插件
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-adguardhome
+rm -rf feeds/luci/applications/luci-app-rclone
+rm -rf feeds/luci/applications/luci-app-passwall
 
 # 1-添加 ShadowSocksR Plus+ 插件
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # 2-添加 PowerOff 关机插件
-git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
+# git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
 # 3-添加 opentomcat 主题
 git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
 # 4-添加 OpenClash 插件
-sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+# sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
 # 5-添加 PassWall 插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 6-添加 adguardhome 插件
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+
+# 7-添加 软件过滤OAF 插件
+git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
